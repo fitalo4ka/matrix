ef2b4c06157f63cc92b8c8c34bcc8d43dca6634f
Natalia Firstova <113723377+fitalo4ka@users.noreply.github.com>
2023-06-10 23:14:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <math.h>
 
 template <typename name>
 class matrix : public std::error_code {
@@ -24,7 +25,6 @@ public:
         }
     }
     matrix(size_t line, size_t column) : line(line), column(column) {
-        // std::vector<std::vector<name>> main_matrix(line, std::vector<int>(column, 0));
         std::vector<name> val;
         for (int i = 0; i < line; ++i) {
             main_matrix.push_back(val);
@@ -36,8 +36,8 @@ public:
 
     matrix(std::string s) {
         std::ifstream file(s);
-        std::string str;
         if (file.is_open()) {
+            std::string str;
             file >> line >> column;
             for(size_t i = 0; i < line; ++i) {
                 std::getline(file, str);
@@ -118,10 +118,100 @@ public:
         return true;
     }
 
+    matrix get_minor (size_t size, size_t l, size_t c) const {
+        if (line != column) {
+            throw std::runtime_error("Matrix is not square");
+        }
+        matrix matrix_1(size - 1, size - 1);
+        int set_row = 0;
+        int set_col = 0;
+        for (size_t i = 0; i < size - 1; i++) {
+            if (i == l) {
+                set_row = 1;
+            }
+            set_col = 0;
+            for (size_t j = 0; j < size - 1; j++) {
+                if (j == c) {
+                    set_col = 1;
+                }
+                matrix_1.main_matrix[i][j] = main_matrix[i + set_row][j + set_col];
+            }
+        }
+        return matrix_1;
+    }
+
+    name determinant(size_t size) const {
+        name det = 0;
+        name sign = 1;
+        if (size == 1) {
+            return main_matrix[0][0];
+        } else if (size == 2) {
+            return (main_matrix[0][0] * main_matrix[1][1] - main_matrix[0][1] * main_matrix[1][0]);
+        }
+        for (size_t j = 0; j < size; j++) {
+            matrix matrix_1 = get_minor(size, 0, j);
+            det = det + (sign * main_matrix[0][j]*matrix_1.determinant(size - 1));
+            sign = -sign;
+        }
+        return det;
+    }
+
+    matrix<name> Transpose() const{
+        matrix<name> transpose_matrix(line, column);
+        for (int i = 0; i < line; ++i) {
+            for (int j = 0; j < column; ++j) {
+                transpose_matrix.main_matrix[i][j] = this->main_matrix[j][i];
+            }
+        }
+        return transpose_matrix;
+    }
+
+
     matrix<name> operator!() const {
         if (line != column) {
             throw std::runtime_error("Matrix is not square");
         }
+        matrix<name> matrix_1(line, column);
+        matrix<name> t = Transpose();
+        for (size_t i = 0; i < line; i++) {
+            for (size_t j = 0; j < column; j++) {
+                matrix_1.main_matrix[i][j] = (t.get_minor(line, i, j)).determinant(line - 1) * pow(-1, i+j);
+            }
+        }
+        matrix<name> b(line, column);
+        b = (matrix_1 * (1/this->determinant(line)));
+        return b;
+    }
+
+    static matrix<name>& zero_matrix(size_t size) {
+        matrix<name> Matrix = new matrix<name>;
+        Matrix.line = size;
+        Matrix.column = size;
+        for (size_t i = 0; i < size; ++i) {
+            std::vector<name> a;
+            for (size_t j = 0; j < size; ++j) {
+                a.push_back(0);
+            }
+            Matrix.main_matrix.push_back(a);
+        }
+        return *Matrix;
+    }
+
+    static matrix<name>& unit_matrix(size_t size) {
+        matrix<name> Matrix = new matrix<name>;
+        Matrix.line = size;
+        Matrix.column = size;
+        for (size_t i = 0; i < size; ++i) {
+            std::vector<name> a;
+            for (size_t j = 0; j < size; ++j) {
+                if (i == j) {
+                    a.push_back(1);
+                }
+                else a.push_back(0);
+            }
+            Matrix.main_matrix.push_back(a);
+        }
+        return *Matrix;
     }
 };
 
@@ -190,6 +280,7 @@ std::ostream &operator<<(std::ostream &out, matrix<type> matrix_1) {
         out << std::endl;
     }
     out << std::endl;
+    return out;
 }
 
 int main() {
@@ -198,6 +289,7 @@ int main() {
     std::cout << "matrix addition: \n" << M + M1;
     std::cout << "matrix multiplication: \n" << M * M1;
     std::cout << "multiplying a matrix by a number: \n" << M * 2;
+    std::cout << !M;
 
     return 0;
 }
